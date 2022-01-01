bb0cad6b8ac20eae10bd0efd1c165040c88b3f2d
Graham Plata <graham.plata@gmail.com>
2021-12-31 20:48:20 -0500

    Migrate to Cobra CLI library (#9)
    * add rust to deprecated folder
    
    * migrate and add logging output

--- go.mod
+++ go.mod
@@ -1,6 +1,27 @@
-module eta
+module eta-multitool
 
 go 1.17
+
+require (
+	github.com/gagliardetto/solana-go v1.0.2
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cobra v1.3.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
 
 require (
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.4 // indirect
@@ -10,33 +31,33 @@ require (
 	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dfuse-io/logging v0.0.0-20210109005628-b97a57253f70 // indirect
-	github.com/fatih/color v1.7.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/gagliardetto/binary v0.5.0 // indirect
-	github.com/gagliardetto/solana-go v1.0.2
 	github.com/gagliardetto/treeout v0.1.4 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
-	github.com/mattn/go-colorable v0.0.9 // indirect
-	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mostynb/zstdpool-freelist v0.0.0-20201229113212-927304c0c3b1 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.1
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
 	github.com/tidwall/gjson v1.6.7 // indirect
 	github.com/tidwall/match v1.0.3 // indirect
 	github.com/tidwall/pretty v1.0.2 // indirect
-	go.opencensus.io v0.22.5 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/ratelimit v0.2.0 // indirect
-	go.uber.org/zap v1.16.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 )
---
+++ pkg/components/token.go
@@ -0,0 +1,107 @@
+package components
+
+// Intentionally verbose as I am still very green when it comes to interacting with the Solana Ecosystem.
+// Mostly streaming thoughts as a go along. Hopefully someone else finds this useful.
+
+import (
+	"context"
+	"eta-multitool/pkg/config"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/sirupsen/logrus"
+)
+
+// BuildProgramAccountOptions Build options to filter program accounts by a specific 'Upgrade Authority' public key
+// A Solana 'Upgrade Authority' is a key that allows for a program account to be upgraded it is required at Deploy time.
+func BuildProgramAccountOptions(pub solana.PublicKey) rpc.GetProgramAccountsOpts {
+	offset := uint64(1)
+	length := uint64(1024)
+	filters := []rpc.RPCFilter{}
+	filters = append(filters, rpc.RPCFilter{
+		Memcmp: &rpc.RPCFilterMemcmp{
+			Offset: 1,
+			Bytes:  pub.Bytes(),
+		},
+	})
+	options := rpc.GetProgramAccountsOpts{
+		Encoding:   solana.EncodingBase64Zstd,
+		Commitment: rpc.CommitmentFinalized,
+		DataSlice: &rpc.DataSlice{
+			Offset: &offset,
+			Length: &length,
+		},
+		Filters: filters,
+	}
+	return options
+}
+
+// GetAllMetaplexTokenByAuthority returns all accounts owned by the provided program publicKey.
+func GetAllMetaplexTokenByAuthority() {
+	// Initialize a client with the mainnet-beta endpoint
+	endpoint := rpc.MainNetBeta_RPC
+	client := rpc.New(endpoint)
+	// Log the endpoint
+	logrus.WithFields(logrus.Fields{"endpoint": endpoint}).Debug("solana rpc client initialized")
+
+	// 'Base58' instead of standard Base64 encoding?
+	// - Don't want 0OIl characters that look the same in some fonts and could be used to create visually identical looking account numbers.
+	// - A string with non-alphanumeric characters is not as easily accepted as an account number.
+	// - E-mail usually won't line-break if there's no punctuation to break at.
+	// - Doubleclicking selects the whole number as one word if it's all alphanumeric.
+	//
+	// Validate 'Base58' Public keys from string constants.
+	// Constants are from Metaplex and GRIM Update Authority
+
+	metaplexProgramKey := solana.MustPublicKeyFromBase58(config.MetaPlexProgramKey)
+	grimUpdateAuthorityKey := solana.MustPublicKeyFromBase58(config.GrimUpdateAuthorityKey)
+
+	// 'Program Accounts' are Solana 'Accounts' marked "executable" in its metadata.
+	//
+	// In the following block we are looking to query against the MetaPlex Candy Machine program account.
+	// We are interested in a specific set of downstream accounts so we will need to make use if the additional options.
+	// This will allow us just to get back accounts specific to a specific Update Authority Key. ('GRIM')
+	programAccountOptions := BuildProgramAccountOptions(grimUpdateAuthorityKey)
+	logrus.WithFields(logrus.Fields{"metaplex_program_key": metaplexProgramKey, "update_authority_key": grimUpdateAuthorityKey}).Debug("fetching program accounts")
+	resp, err := client.GetProgramAccountsWithOpts(context.Background(), metaplexProgramKey, &programAccountOptions)
+	// Handle the error
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"error": err}).Error("unable to get program accounts")
+	}
+	logrus.WithFields(logrus.Fields{"count": len(resp)}).Debug("got program accounts")
+
+	// NOTE: This is where the layers of the onion started revealing themselves.
+	//
+	// I came to understand the linking of the Solana components much better when I walked through them with
+	// the https://solscan.io/ tool. I attempted to trace the same process via the UI in the following loop.
+
+	// Now that we have a response containing all accounts owned by the program publicKey we need to handle them. (10k+) at the time of writing.
+	for i, value := range resp {
+		logrus.WithFields(logrus.Fields{"index": i, "public_key": value.Pubkey}).Debug("fetching signatures for address")
+
+		// 'Signatures' are lists of all account public keys referenced by a transaction's instructions.
+		// We are interested in this because we want the first Signature
+		//
+		// Given an individual account lets fetch the 'Signatures' from the Account public key
+		sigs, err := client.GetSignaturesForAddress(context.Background(), value.Pubkey)
+		// Handle the error
+		if err != nil {
+			logrus.WithFields(logrus.Fields{"index": i, "error": err, "public_key": value.Pubkey.String()}).Error("unable to get signatures for account public key")
+		}
+		// Get the first 'Signature' when the token would have been created
+		genesis_signature := sigs[len(sigs)-1].Signature
+		logrus.WithFields(logrus.Fields{"index": i, "genesis_signature": genesis_signature, "count": len(sigs), "public_key": value.Pubkey}).Debug("got address signatures")
+
+		// 'Transactions' are instruction(s) signed by a client using one or more keypairs and executed atomically
+		//
+		// Get the 'Transaction' from the Genesis (first) 'Signature' passing in empty/default options
+		tx, err := client.GetTransaction(context.Background(), genesis_signature, &rpc.GetTransactionOpts{})
+		if err != nil {
+			logrus.WithFields(logrus.Fields{"index": i, "error": err, "public_key": value.Pubkey.String()}).Error("unable to get signatures for account public key")
+		}
+		// Parse all 'Account' keys keeping the second entry as that will be our NFT + Metadata Account
+		account_keys := tx.Transaction.GetParsedTransaction().Message.AccountKeys
+		token_address := account_keys[1]
+		logrus.WithFields(logrus.Fields{"index": i, "token_address": token_address, "public_key": value.Pubkey}).Info(token_address)
+	}
+}

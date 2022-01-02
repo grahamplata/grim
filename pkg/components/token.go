63472a685b82fbf370ef247a4226ba4013cb3265
Graham Plata <graham.plata@gmail.com>
2022-01-02 16:57:29 -0500

    Concurrency and Rate Limiting (#11)
    * split
    
    * concurrency and rate limiting

--- pkg/components/token.go
+++ pkg/components/token.go
@@ -6,6 +6,7 @@ package components
 import (
 	"context"
 	"eta-multitool/pkg/config"
+	"time"
 
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
@@ -75,33 +76,103 @@ func GetAllMetaplexTokenByAuthority() {
 	// I came to understand the linking of the Solana components much better when I walked through them with
 	// the https://solscan.io/ tool. I attempted to trace the same process via the UI in the following loop.
 
-	// Now that we have a response containing all accounts owned by the program publicKey we need to handle them. (10k+) at the time of writing.
-	for i, value := range resp {
-		logrus.WithFields(logrus.Fields{"index": i, "public_key": value.Pubkey}).Debug("fetching signatures for address")
+	// Concurrency Block and https://docs.solana.com/cluster/rpc-endpoints#mainnet-beta
 
-		// 'Signatures' are lists of all account public keys referenced by a transaction's instructions.
-		// We are interested in this because we want the first Signature
-		//
-		// Given an individual account lets fetch the 'Signatures' from the Account public key
-		sigs, err := client.GetSignaturesForAddress(context.Background(), value.Pubkey)
-		// Handle the error
-		if err != nil {
-			logrus.WithFields(logrus.Fields{"index": i, "error": err, "public_key": value.Pubkey.String()}).Error("unable to get signatures for account public key")
-		}
-		// Get the first 'Signature' when the token would have been created
-		genesis_signature := sigs[len(sigs)-1].Signature
-		logrus.WithFields(logrus.Fields{"index": i, "genesis_signature": genesis_signature, "count": len(sigs), "public_key": value.Pubkey}).Debug("got address signatures")
+	// length of Program accounts that will need token accounts fetched
+	totalAccounts := len(resp)
 
-		// 'Transactions' are instruction(s) signed by a client using one or more keypairs and executed atomically
-		//
-		// Get the 'Transaction' from the Genesis (first) 'Signature' passing in empty/default options
-		tx, err := client.GetTransaction(context.Background(), genesis_signature, &rpc.GetTransactionOpts{})
-		if err != nil {
-			logrus.WithFields(logrus.Fields{"index": i, "error": err, "public_key": value.Pubkey.String()}).Error("unable to get signatures for account public key")
-		}
-		// Parse all 'Account' keys keeping the second entry as that will be our NFT + Metadata Account
-		account_keys := tx.Transaction.GetParsedTransaction().Message.AccountKeys
-		token_address := account_keys[1]
-		logrus.WithFields(logrus.Fields{"index": i, "token_address": token_address, "public_key": value.Pubkey}).Info(token_address)
+	// the number of goroutines that are allowed to run concurrently TODO: move to config
+	maxConcurrentGoroutines := 3
+
+	// GO Channel to coordinate the number of concurrent goroutines.
+	// This channel should be buffered otherwise we will be immediately blocked when trying to fill it.
+	concurrentGoroutines := make(chan struct{}, maxConcurrentGoroutines)
+	// Fill channel with maxNbConcurrentGoroutines empty struct.
+	for i := 0; i < maxConcurrentGoroutines; i++ {
+		concurrentGoroutines <- struct{}{}
 	}
+
+	// done channel indicates when a goroutine has finished its job
+	done := make(chan bool)
+	// waitForAllJobs channel allows the main program to wait until we done all the jobs
+	waitForAllJobs := make(chan bool)
+
+	// Collect all the jobs, and since the job is finished, we can
+	// release another spot for a goroutine.
+	go func() {
+		for i := 0; i < totalAccounts; i++ {
+			<-done
+			// Say that another goroutine can now start.
+			concurrentGoroutines <- struct{}{}
+		}
+		// We have collected all the jobs, the program
+		// can now terminate
+		waitForAllJobs <- true
+	}()
+
+	for i := 1; i < totalAccounts; i++ {
+		// Try to receive from the concurrentGoroutines channel. When we have something, it means we can start a new goroutine because another one finished.
+		// Otherwise, it will block the execution until an execution spot is available.
+		<-concurrentGoroutines
+		go func(i int) {
+			// lazy ratelimit
+			time.Sleep(900 * time.Millisecond)
+			// Now that we have a response containing all accounts owned by the program publicKey we need to handle them. (10k+) at the time of writing.
+			pubkey := resp[i].Pubkey
+			genesis_signature := GetGenesisSignatureForAddress(client, pubkey)
+			token := GetTokenFromTransaction(client, genesis_signature, pubkey)
+			logrus.WithFields(logrus.Fields{
+				"index":         i,
+				"token_address": token,
+				"public_key":    pubkey},
+			).Info(token)
+			done <- true
+		}(i)
+	}
+	// Wait for all jobs to finish
+	<-waitForAllJobs
+}
+
+// GetTokenFromTransaction
+func GetTokenFromTransaction(client *rpc.Client, gen_sig solana.Signature, pubkey solana.PublicKey) solana.PublicKey {
+	// 'Transactions' are instruction(s) signed by a client using one or more keypairs and executed atomically
+	//
+	// Get the 'Transaction' from the Genesis (first) 'Signature' passing in empty/default options
+	tx, err := client.GetTransaction(context.Background(), gen_sig, &rpc.GetTransactionOpts{})
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":   err,
+			"pub_key": pubkey.String(),
+		}).Error("unable to get transactions for account public key")
+	}
+	// Parse all 'Account' keys keeping the second entry as that will be our NFT + Metadata Account
+	account_keys := tx.Transaction.GetParsedTransaction().Message.AccountKeys
+	return account_keys[1]
+}
+
+// GetGenesisSignatureForAddress
+func GetGenesisSignatureForAddress(client *rpc.Client, pubkey solana.PublicKey) solana.Signature {
+	logrus.WithFields(logrus.Fields{
+		"public_key": pubkey,
+	}).Debug("fetching signatures for address")
+	// 'Signatures' are lists of all account public keys referenced by a transaction's instructions.
+	// We are interested in this because we want the first Signature
+	//
+	// Given an individual account lets fetch the 'Signatures' from the Account public key
+	sigs, err := client.GetSignaturesForAddress(context.Background(), pubkey)
+	// Handle the error
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":      err,
+			"public_key": pubkey.String(),
+		}).Error("unable to get signatures for account public key")
+	}
+	// Get the first 'Signature' when the token would have been created
+	genesis_signature := sigs[len(sigs)-1].Signature
+	logrus.WithFields(logrus.Fields{
+		"genesis_signature": genesis_signature,
+		"count":             len(sigs),
+		"public_key":        pubkey,
+	}).Debug("got address signatures")
+	return genesis_signature
 }
